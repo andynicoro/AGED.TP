a0c2eb307fa5cf8b4d84a14d905410fe445433a7
emimartindlu2@gmail.com <emimartindlu2@gmail.com>
2019-07-22 19:15:37 -0300

    arreglo prueba

--- TpAGED/Main.cpp
+++ TpAGED/Main.cpp
@@ -101,7 +101,7 @@ int main() {
 			}
 			break;
 		default : system("cls");
-			cout << "Ingrese un valor correct" << endl;
+			cout << "Ingrese un valor correctos" << endl;
 		}
 	} while (aux != 's' || aux != 'S');
 
